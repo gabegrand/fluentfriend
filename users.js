d32051f2dec7d381256d6ee3a89e97d6dfdb876a
Gabriel Grand <ggrand@college.harvard.edu>
2018-04-17 21:38:19 -0400

    UX/design cosmetic changes

--- users.js
+++ users.js
@@ -21,20 +21,20 @@ $( document ).ready(function() {
 });
 
 function buildProf(obj) {
-    return `<div class="col-md-4 mb-3">
-              <div class="profile centerBlock pb-3">
-                <span class="badge badge-pill badge-primary">${obj.mins} mins</span>
-                <br/>
-                <img src="media/${obj.img}" height="250px" width="250px" class="img-fluid" />
-                <h4>${obj.name}</h4>
-                <p>${obj.location}</p>
-                <button type="button" class="btn btn-primary user_button" onclick="addScheduled(${obj.uid})">Schedule conversation</button>
-                <button class="btn btn-info user_button" type="button" data-toggle="collapse" data-target="#collapseExample${obj.uid}" aria-expanded="false" aria-controls="collapseExample">
-                  About ${obj.name}
-                </button>
-                <div class="collapse" id="collapseExample${obj.uid}">
-                  <div class="card card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+    return `<div class="profile col-md-4 mb-3">
+              <div class="card text-center" style="width: 20rem;">
+                <img class="card-img-top" src="media/${obj.img}" alt="Card image cap">
+                <div class="card-body">
+                  <h4 class="card-title">${obj.name}</h4>
+                  <p class="card-text">${obj.location}</p>
+                  <button type="button" class="btn btn-primary user_button" onclick="addScheduled(${obj.uid})">Schedule conversation</button>
+                  <button class="btn btn-info user_button" type="button" data-toggle="collapse" data-target="#collapseExample${obj.uid}" aria-expanded="false" aria-controls="collapseExample">
+                    About ${obj.name}
+                  </button>
+                  <div class="collapse" id="collapseExample${obj.uid}">
+                    <div class="card card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                    </div>
                   </div>
                 </div>
               </div>
--- video_call.js
+++ video_call.js
@@ -7,15 +7,12 @@ $( document ).ready(function() {
     }
 
     let partnerPic = document.getElementById("partnerPic");
-    partnerPic.innerHTML = `<img src="media/${curUser.img}" height="350px" width="350px" class="img-fluid" />`
+    partnerPic.innerHTML = `<img src="media/${curUser.img}" height="100px" width="100px" class="img-fluid" />`
 
     let partnerInfo = document.getElementById("partnerInfo");
     partnerInfo.innerHTML = `<h4>${curUser.name}</h4>
                             <p>Location: ${curUser.location}</p>
                             <button type="button" class="btn btn-primary"><i class="fa fa-commenting-o" aria-hidden="true"></i></button>
                             <button type="button" class="btn btn-primary m"><i class="fa" aria-hidden="true"></i>I'm Stuck!</button>
-                            <button type="button" class="btn btn-danger ml-4" onclick="location.href='feedback.html';"><i class="fa fa-phone" aria-hidden="true"></i>  End Call</button>
-                            <div class="row-md-8 formBlock">
-                                <button type="button" class="btn btn-danger"><i class="fa fa-flag" aria-hidden="true"></i>  Report Inappropriate Call</button>
-                            </div>`
+                            <button type="button" class="btn btn-danger ml-4" onclick="location.href='feedback.html';"><i class="fa fa-phone" aria-hidden="true"></i>  End Call</button>`
 });
