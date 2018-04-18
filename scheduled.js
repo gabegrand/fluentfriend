c7acaa27392e3eba8f4609ff892cc98fd4b77cf8
Gabriel Grand <ggrand@college.harvard.edu>
2018-04-17 21:59:19 -0400

    More UX changes

--- scheduled.js
+++ scheduled.js
@@ -12,17 +12,17 @@ $(document).ready(function() {
 });
 
 function buildScheduledProf(obj) {
-  return `<div class="row border-bottom pb-4">
+  return `<div class="row border-bottom pb-4 justify-content-center">
 		<div class="col-md-3 profile p">
-			<img src="media/${obj.img}" class="img-fluid" />
+			<img src="media/${obj.img}" class="img-fluid rounded" />
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-3">
 			<h4>${obj.name}</h4>
 			<p>Location: ${obj.location}</p>
 			<button type="button" class="btn btn-primary"><i class="fa fa-commenting-o" aria-hidden="true"></i>  Send message</button>
 		</div>
-		<div class="col-md-3">
-			<div class="rightBlock formBlock profile p-3">
+		<div class="col-md-4">
+			<div class="formBlock p-3 darkbox">
 				<p>Conversation Date & Time</p>
 				<div>
 					<i class="fa fa-calendar" aria-hidden="true"></i>
@@ -32,9 +32,9 @@ function buildScheduledProf(obj) {
 					<i class="fa fa-clock-o" aria-hidden="true"></i>
 					<input size="18" type="text" value="02:30 PM" readonly class="form_datetime" id="time_${obj.uid}">
 				</div>
-				<div><button type="button" class="btn btn-primary" onclick="rescheduleCall(${obj.uid})"><i class="fa fa-repeat" aria-hidden="true"></i>   Propose new date & time</button></div>
-				<div><button type="button" class="btn btn-success" onclick="startCall(${obj.uid})"><i class="fa fa-phone" aria-hidden="true"></i>   Call now!</button></div>
-				<div><button type="button" class="btn btn-danger" onclick="cancelCall(${obj.uid})"><i class="fa fa-times" aria-hidden="true"></i>   Cancel call</button></div>
+				<div><button type="button" class="btn btn-primary user_button" onclick="rescheduleCall(${obj.uid})"><i class="fa fa-repeat" aria-hidden="true"></i>   Propose new date & time</button></div>
+				<div><button type="button" class="btn btn-success user_button" onclick="startCall(${obj.uid})"><i class="fa fa-phone" aria-hidden="true"></i>   Call now!</button></div>
+				<div><button type="button" class="btn btn-danger user_button" onclick="cancelCall(${obj.uid})"><i class="fa fa-times" aria-hidden="true"></i>   Cancel call</button></div>
 			</div>
 		</div>
 	</div>`;
