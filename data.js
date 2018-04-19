373671a2fe2f49543ab230b645bb01492d3d4971
Mason Hale <mason.p.hale@gmail.com>
2018-04-18 20:45:11 -0400

    about sections filled out

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ const USERS = [
     role: "speaker",
     location: "Cambridge, MA",
     img: "sr_sheila.jpg",
+    about: "I'm a retired elementary school teacher (second grade!), and I love meeting and talking to new people. My main hobbies are singing, playing the piano, and gardening. I have two adorable cats named Mario and Luigi, and they would love to meet you!!",
     mins: 66
   },
   {
@@ -13,6 +14,7 @@ const USERS = [
     role: "speaker",
     location: "Dallas, TX",
     img: "sr_walter.jpg",
+    about: "Just a guy who likes good conversation. I'm a big sports fan, especially football and basketball (I don't play anymore but watch religiously!). I also enjoy cooking--I'm always learning new recipes to wow visitors.",
     mins: 499
   },
   {
@@ -21,6 +23,7 @@ const USERS = [
     role: "learner",
     location: "New York, NY",
     img: "esl_sonia.jpg",
+    about: "I moved to the US seven years ago and have been working on my English ever since. I speak fluent Spanish as I am from El Salvador; I like to visit home at least once a year. My biggest loves are fashion and new technology.",
     mins: 261
   },
   {
@@ -29,6 +32,7 @@ const USERS = [
     role: "learner",
     location: "Seattle, WA",
     img: "esl_jerry.jpg",
+    about: "Hello! I am Jerry, and I was born and raised in Beijing, China! I like dogs, watching the NFL, and hiking. My English is only okay, but I am very excited to learn more!",
     mins: 300
   },
   {
@@ -37,6 +41,7 @@ const USERS = [
     role: "speaker",
     location: "Salem, OR",
     img: "sr_david.jpg",
+    about: "Hi there. I am a retired professor of anthropology (at the University of Washington), and I am most interested in learning about other cultures. I travel as often as I can, which these days is about twice a year.",
     mins: 81
   },
   {
@@ -45,6 +50,7 @@ const USERS = [
     role: "speaker",
     location: "Bridgeport, CT",
     img: "sr_joanna.jpg",
+    about: "I'm Joanna! In my younger years, I was a policewoman, but now I mostly like to volunteer and talk to my kids over the phone. I'm so excited to talk to new friends, and I can't wait to meet you!",
     mins: 495
   },
   {
@@ -53,6 +59,7 @@ const USERS = [
     role: "speaker",
     location: "New Orleans, LA",
     img: "sr_jason.jpg",
+    about: "Hello! I'm Jason, and I used to be an accountant. Nowadays, I enjoy playing golf and tennis, and I also love reading science fiction in my free time. Pleased to be here!",
     mins: 440
   },
   {
@@ -61,6 +68,7 @@ const USERS = [
     role: "learner",
     location: "Mexico City, Mexico",
     img: "esl_juan.jpg",
+    about: "I'm Juan, and I've been learning English for ten years. I'm far from perfect, but I consider myself nearly fluent! I love riding my bike, especially with my wife Laura, as well as following world politics.",
     mins: 198
   },
   {
@@ -69,6 +77,7 @@ const USERS = [
     role: "learner",
     location: "Los Angeles, CA",
     img: "esl_julia.jpg",
+    about: "Hi, I am Julia from Guadalajara, Mexico! I now learn English for two years and love to speak to Americans! I like to read history and swim at the beach. Excited to talk!!",
     mins: 423
   },
   {
@@ -77,6 +86,7 @@ const USERS = [
     role: "learner",
     location: "Shanghai, China",
     img: "esl_liwei.jpg",
+    about: "I am Li Wei from China, I am taking English for two years in university now. I like reading and drinking coffee! I have a beautiful wife, and we love going out to movies and shows.",
     mins: 321
   },
   {
@@ -85,6 +95,7 @@ const USERS = [
     role: "learner",
     location: "Nairobi, Kenya",
     img: "esl_asha.jpg",
+    about: "Hi! I'm Asha. I speak pretty fluent English and am looking to practice with a friendly partner! I finished university last year and am currently working as a paralegal in Nairobi. I have a pet fish named Nemo, who is awesome.",
     mins: 95
   },
   {
@@ -93,6 +104,7 @@ const USERS = [
     role: "speaker",
     location: "Ontario, Canada",
     img: "sr_ellen.jpg",
+    about: "Hiya! I'm Ellen, and I have four kids who are off doing their own thing (too busy for mom - ha!). I like to go on walks and knit for my grandkids. Would love to talk!",
     mins: 66
   },
 ];
