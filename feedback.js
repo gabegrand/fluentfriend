ccc50278ed7aed1891b6d64552d94391b9b7c9ea
Mason Hale <mason.p.hale@gmail.com>
2018-04-24 15:39:30 -0400

    changed feedback so that only learners rate partners on patience

--- feedback.js
+++ feedback.js
@@ -1,5 +1,16 @@
 let reportedUsers = []
 
+$(document).ready(function() {
+    let user_role = localStorage.getItem('user_role');
+    if (user_role == 'learner'){
+        let learnerQuestion = document.getElementById("learnerQuestion");
+        learnerQuestion.innerHTML =
+        `<p class="lead">Please rate your partner on their patience.</p>
+        <input type="range" min="1" max="100" value="50" class="slider" id="ratePatience" oninput="patienceOutputId.value = Math.round(ratePatience.value/10)">
+        <output name="patienceOutput" id="patienceOutputId">5</output>`;
+    }
+  });
+
 function reSchedule(){
     let selection = document.querySelector('input[name = "feedback"]:checked').value;
     console.log(selection);
