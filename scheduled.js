fe0a1b1e70535c7b6f62b559e78fba2bf1a9e822
Maria Zlatkova <zlatkova@college.harvard.edu>
2018-04-25 02:11:12 -0400

    reschedule/confirm date and time

--- scheduled.js
+++ scheduled.js
@@ -1,4 +1,5 @@
 let scheduledUsers = [];
+let convDateTime = "";
 
 $(document).ready(function() {
   let scheduledState = localStorage.getItem('scheduled_users');
@@ -8,18 +9,14 @@ $(document).ready(function() {
 
   let scheduledList = document.getElementById("scheduledList");
   scheduledList.innerHTML = USERS.filter(u => scheduledUsers.includes(u.uid)).map(u => buildScheduledProf(u)).join('');
-	console.log(scheduledUsers);
-	for (let uid in scheduledUsers) {
-		console.log('#datetimepicker_' + scheduledUsers[uid]);
-		$('#datetimepicker_' + scheduledUsers[uid]).datetimepicker({
-			defaultDate: (new Date().getMonth() + 1) + "/" + (new Date().getDate() + 1) + "/" + new Date().getFullYear(),
-		});
 
-		$('#datetimepicker_' + scheduledUsers[uid]).on("change.datetimepicker", function (e) {
-        // $('#datetimepicker_' + scheduledUsers[uid]).datetimepicker('minDate', e.date);
-				console.log(e.date);
-    });
-	}
+	$('[data-toggle="tooltip"]').tooltip({
+    trigger : 'hover'
+	});
+
+	$("#reschedule_btn").on('click', function () {
+	    $(this).tooltip('hide')
+	});
 });
 
 function buildScheduledProf(obj) {
@@ -36,21 +33,50 @@ function buildScheduledProf(obj) {
 			<div class="formBlock p-3 darkbox">
 				<p>Conversation Date & Time</p>
 
-				<div class="form-group user_button">
-                <div class="input-group date" id="datetimepicker_${obj.uid}" data-target-input="nearest">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker_${obj.uid}"/>
-                    <div class="input-group-append" data-target="#datetimepicker_${obj.uid}" data-toggle="datetimepicker">
-                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                    </div>
-										<button type="button" class="btn btn-success btn-check"><i class="fa fa-check" aria-hidden="true"></i></button>
-                </div>
-            </div>
+				<div id="conversationdatetime_${obj.uid}">
+					<span data-toggle="tooltip" title="We've scheduled the conversation based on when you're both free!">04/25/2018 04:00 PM</span>
+					<button type="button" id="reschedule_btn" class="btn btn-primary btn-check" data-toggle="tooltip" title="Click here to reschedule conversation" onclick="reschedule(${obj.uid})"><i class="fa fa-repeat" aria-hidden="true"></i></button>
+				</div>
+
 
 				<div><button type="button" class="btn btn-success user_button" onclick="startCall(${obj.uid})"><i class="fa fa-phone" aria-hidden="true"></i>   Call now!</button></div>
 				<div><button type="button" class="btn btn-danger user_button" onclick="cancelCall(${obj.uid})"><i class="fa fa-times" aria-hidden="true"></i>   Cancel call</button></div>
 			</div>
 		</div>
 	</div>`;
+}
+
+function reschedule(uid) {
+	let conversationDateTime = document.getElementById("conversationdatetime_" + uid);
+  conversationDateTime.innerHTML =
+	`<div class="form-group user_button">
+		<div class="input-group date" id="datetimepicker_${uid}" data-target-input="nearest" >
+				<input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker_${uid}" id="pickertext_${uid}" data-toggle="tooltip" title="We've scheduled the conversation based on when you're both free!"/>
+				<div class="input-group-append" data-target="#datetimepicker_${uid}" data-toggle="datetimepicker">
+						<div class="input-group-text"><i class="fa fa-calendar"></i></div>
+				</div>
+				<button type="button" id="confirm_btn" class="btn btn-success btn-check" data-toggle="tooltip" title="Click here to confirm date and time" onclick="confirm(${uid})"><i class="fa fa-check" aria-hidden="true"></i></button>
+		</div>
+	</div>`;
+
+	$('#datetimepicker_' + uid).datetimepicker({});
+
+	$('#datetimepicker_' + uid).on("change.datetimepicker", function (e) {
+			convDateTime = (e.date._d.getMonth() < 10 ? "0" : "") +
+			(e.date._d.getMonth() + 1) + "/" + (e.date._d.getDate() < 10 ? "0" : "") +
+			e.date._d.getDate() + "/" + e.date._d.getFullYear() + " " +
+			(e.date._d.getHours() < 10 ? "0" : "") + (e.date._d.getHours() +
+			(e.date._d.getHours() > 12 ? -12 : 0)) + ":" +
+			(e.date._d.getMinutes() < 10 ? "0" : "") + e.date._d.getMinutes() + " " +
+			(e.date._d.getHours() > 12 ? "PM" : "AM");
+	});
+}
+
+function confirm(uid) {
+	let conversationDateTime = document.getElementById("conversationdatetime_" + uid);
+  conversationDateTime.innerHTML =
+		`<span>${convDateTime}</span>
+		<button type="button" id="reschedule_btn" class="btn btn-primary btn-check" data-toggle="tooltip" title="Click here to reschedule conversation" onclick="reschedule(${uid})"><i class="fa fa-repeat" aria-hidden="true"></i></button>`;
 }
 
 function startCall(uid) {
